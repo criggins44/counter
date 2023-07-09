d36966b68fcc01a05f7f4d169b39229ec08279c6
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-07-09 13:43:04 -0400

    added functons to execute on click

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //Build a counter
 
 let val = document.getElementById('counter');
-let value = 0
+let value = 0;
+
+function increaseCounter(){
+	value += 1;
+	val.innerHTML = value;
+}
+
+function decreaseCounter(){
+	value -= 1;
+	val.innerHTML = value;
+}
+
+function resetCounter(){
+	value = 0;
+	val.innerHTML = value;
+}
